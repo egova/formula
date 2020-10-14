d2db727cb4e88b4a4bde58ab0ca891c2671bffcd
王漩波 <1345545983@qq.com>
2020-10-14 17:17:13 +0800

    :sparkles: 反射调用方法时猜测参数

--- src/main/java/tk/fishfish/formula/reflect/Invocation.java
+++ src/main/java/tk/fishfish/formula/reflect/Invocation.java
@@ -13,13 +13,24 @@ public class Invocation {
 
     private final Method method;
     private final Object target;
+    private final int parameterCount;
 
     public Invocation(Method method, Object target) {
         this.method = method;
         this.target = target;
+        this.parameterCount = method.getParameterCount();
     }
 
-    public Object invoke(Object... args) throws InvocationTargetException, IllegalAccessException {
+    public Object invoke(Object[] args) throws InvocationTargetException, IllegalAccessException {
+        // 如果方法没有参数，则忽略传递的参数
+        if (this.parameterCount == 0) {
+            return method.invoke(target);
+        }
+        // 如果方法只有一个参数，则将参数数组作为一个值传递
+        if (this.parameterCount == 1) {
+            return method.invoke(target, new Object[]{args});
+        }
+        // 如果方法有多个参数，则将参数数组传递，按位匹配
         return method.invoke(target, args);
     }
 
